dd4008a8989bc4ebbf86f7256675c4a6beeb3469
tvasiua <tvasiua@gmail.com>
2020-07-08 14:26:26 +0000

    go

---
+++ js/indexx.js
@@ -0,0 +1,11 @@
+document.body.onload = function() {
+
+    setTimeout(function() {
+var loadwrapp = document.getElementById('page-preloader');
+if( !loadwrapp.classList.contains.add('done') );
+{
+ loadwrapp.classList.add('done');
+}
+    }, 1000);
+
+}
